b6c387ea04fbdd2472d84772c4639db9e0835ff6
Ayush123rajawat <97392316+Ayush123rajawat@users.noreply.github.com>
2022-11-10 06:10:49 +0530

    Update app.js
    Created the server

---
+++ app.js
@@ -0,0 +1,18 @@
+const express=require("express");
+const bodyParser=require("body-parser");
+const mongoose=require("mongoose");
+const ejs=require("ejs");
+const { stringify } = require("querystring");
+const { rmSync } = require("fs");
+const app =express();
+app.use(bodyParser.urlencoded({extended:true}));
+app.set('view engine','ejs');
+app.use(express.static("public"));
+mongoose.connect("mongodb://127.0.0.1/SignupDB",{useNewUrlParser:true});
+const db=mongoose.connection;
+const http = require('http').createServer(app);
+const PORT = process.env.PORT || 4000
+
+http.listen(4000,() => {
+    console.log(`port running at ${PORT}`);
+})
